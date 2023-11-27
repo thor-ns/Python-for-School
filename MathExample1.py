d9a3add75267bfaf38521af38c7b1c2c29bf1827
thor-ns <102545045+thor-ns@users.noreply.github.com>
2023-11-27 11:04:06 -0800

    Add files via upload

---
+++ MathExample1.py
@@ -0,0 +1,15 @@
+# Arithmetic operators
+
+# import Math library full of functions
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Display the arithmetic operations available to you in Python
+print(num1, "+", num2, "is", num1 + num2)
+print(num1, "-", num2, "is", num1 - num2)
+print(num1, "/", num2, "is", num1 / num2)
+print(num1, "*", num2, "is", num1 * num2)
+print(num1, "%", num2, "is", num1 % num2)
+print(num1, "/", num2, "is", num1 / num2)
+print(num1, "//", num2, "is",num1 // num2)
