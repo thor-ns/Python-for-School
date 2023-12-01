179a18f779fe7ac283e0ad565781bb10fcf74ddc
thor-ns <dominicklackovic9@gmail.com>
2023-11-30 16:29:22 -0800

    modified:   QuadraticEquation.py

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -1,12 +1,33 @@
 from math import *
+
+#a = input("Enter a: ")
+#b = input("Enter b: ")
+#c = input("Enter c: ")
+
 def positive_quadratic_equation(a, b, c):
     if c > 0:
-        print(str(a) + "x**2 + " + str(b) + "x +" + str(c))
+        print(str(a) + "x^2 + " + str(b) + "x +" + str(c))
     else:
-        print(str(a) + "x**2 + " +  str(b) + "x " + str(c))
+        print(str(a) + "x^2 + " +  str(b) + "x " + str(c))
 
-    """if a == 0:
-        print("Error, number must not be equal to 0")"""
-    return(-b + sqrt(((b**2) - (4*a*c))) / (2*a))
+    if a == 0:
+        print("Error, number must not be equal to 0")
+        exit()
     
-print(positive_quadratic_equation(3, 5, -7))
+    root1 = (-b + sqrt(((b**2) - (4*a*c)))) / (2*a)
+    print("the first root is " + str(root1))
+    
+def negative_quadratic_equation(a, b, c):
+
+    if a == 0:
+        print("Error, number must not be equal to 0")
+        exit()
+    root2 = (-b - sqrt(((b**2) - (4*a*c)))) / (2*a)
+    print("The second root is " + str(root2))
+
+positive_quadratic_equation()
+positive_quadratic_equation()
+positive_quadratic_equation()
+
+#positive_quadratic_equation(int(a), int(b), int(c))
+#negative_quadratic_equation(int(a), int(b), int(c))
