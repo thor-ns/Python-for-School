ca8c1c80d66ad5c59e91583aad93bebad6243664
thor-ns <dominicklackovic9@gmail.com>
2023-12-17 15:50:36 -0800

    reduced line code and changed root function to just square root. Still needs testing

--- Calculator_Project.py
+++ Calculator_Project.py
@@ -25,10 +25,9 @@ def second_greeting():
         x = input("Enter your first number: ")
         y = input("Enter your second number: ")
         power(x, y)
-    elif operation == "root":
+    elif operation == "square root":
         x = input("Enter your first number: ")
-        y = input("Enter your second number: ")
-        root(x, y)
+        squareRoot(x)
     elif operation == "factorial":
         x = input("Which number do you want to find the factorial of: ")
         factorial(x)   
@@ -49,7 +48,7 @@ def second_greeting():
         help()
     else:
         print("Error, not a function of the calculator")
-        exit()
+        second_greeting()
 
 def help():
     question = input("Which function do you need help with? ")
@@ -79,10 +78,9 @@ def help():
         print("puts the first number to the power of the second number (first ^ second)")
         print("""Restrictions: first number must be an integer
                                second number must be a whole number""")
-    elif question == "root":
-        print("puts the first number to the root of the second number (second√first)")
-        print("""Restrictions: first number must be an integer
-                               second number must be a whole number""")
+    elif question == "square root":
+        print("finds the square root of the first number")
+        print("""Restrictions: first number must be an integer""")
     elif question == "factorial":
         print("finds the factorial the number given ex. 20x19x18...3x2x1")
         print("""Restrictions: number must be a POSITIVE whole number""")
@@ -101,6 +99,7 @@ def addition(x, y):
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #addition function
 
 def subtraction(x, y):
     try:
@@ -111,6 +110,7 @@ def subtraction(x, y):
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #subtraction function
 
 
 def multiply(x, y):
@@ -122,6 +122,7 @@ def multiply(x, y):
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #multiplication function
 
 def division(x, y):
     try:
@@ -134,6 +135,7 @@ def division(x, y):
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #division function
 
 def MOD(x, y):
     try:
@@ -146,46 +148,54 @@ def MOD(x, y):
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #remainder function
 
 def power(x, y):
     try:
         x = float(x)
-        y = int(y)
-        pow = x ** y
-        print(f"{x} to the power of {y} is equal to {pow}")
+        y = y.split("/")
+        p = float(y[0])
+        q = float(y[1])
+        pow = x ** (p/q)
+        print(f"{x} to the power of {p} / {q} is equal to {pow}")
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #power function
 
-def root(x, y):
+def squareRoot(x):
     try:
         x = float(x)
-        y = int(y)
-        root = x ** (1 / y)
-        print(f"{x} root {y} is equal to {root}")
-    except:
+        root = x ** (1 / 2)
+        if type(root) == type(1j):
+            print("The answer is a complex number")
+        else:
+            print(f"The square root of {x} is equal to {root}")
+    except TypeError:
         print("Error, must be a numeric value")
     second_greeting()
+    #square root function
 
 def factorial(x):
     try:
         x = int(x)
-    except:
+        if x < 0:
+            print("Error, must be 0 or a higher integer")
+            second_greeting()
+        elif x == 0:
+            print(f"The factorial of 0 is 1")
+            second_greeting()
+        else:
+            lim = 1
+            output = x
+            while lim < x:
+                output = output * lim 
+                lim += 1
+            print(f"The factorial of {x} is {output}") 
+    except TypeError:
         print("Error, must be a numeric value")
-        exit()
-    if x < 0:
-        print("Error, must be 0 or a higher integer")
-        exit()
-    if x == 0:
-        print(f"The factorial of 0 is 1")
-    else:
-        lim = 1
-        output = x
-        while lim < x:
-            output = output * lim 
-            lim += 1
-        print(f"The factorial of {x} is {output}") 
     second_greeting()  
+    #factorial function
 
 def GCF(x, y):
     try:
@@ -197,73 +207,23 @@ def GCF(x, y):
             print(f"The GCF of {x} and {y} is {y}")
         elif y == 0:
             print(f"The GCF of {x} and {y} is {x}")
+        elif x == -abs(x):
+            print("Please enter positive values")
         else:
-            if x or y == -abs(x) or -abs(y):
-                print("Please enter positive values")
-            else:
-                place = x
-                while place > 0:
-                    if x % place == 0 and y % place == 0:
-                        commonFactor = place
-                        print(f"the common factor of {x} and {y} is {commonFactor}")
-                        break
-                    place -= 1
+            place = x
+            while place > 0:
+                if x % place == 0 and y % place == 0:
+                    commonFactor = place
+                    print(f"the common factor of {x} and {y} is {commonFactor}")
+                    break
+                place -= 1
     except:
         print("Error, please enter a numeric value")
     second_greeting()
+    #greatest common factor function
 #end of all mathematical functions 
 
-print("""Welcome to the calculator, the functions available are: addition, subtraction, division, multiplication, MOD/remainder, power, root, 
+print("""Welcome to the calculator, the functions available are: addition, subtraction, division, multiplication, MOD, power, square root, 
 factorial""")
 print("If you need help with any of the functions type \"help\"")
-operation = input("Which function of the calculator do you want to use?: ")
-if operation == "addition":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    addition(x, y)
-elif operation == "subtraction":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    subtraction(x, y)
-elif operation == "multiplication":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    multiply(x, y)
-elif operation == "division":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    division(x, y)
-elif operation == "MOD":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    MOD(x, y)
-elif operation == "power":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    power(x, y)
-elif operation == "root":
-    x = input("Enter your first number: ")
-    y = input("Enter your second number: ")
-    root(x, y)
-elif operation == "factorial":
-    x = input("Which number do you want to find the factorial of: ")
-    factorial(x) 
-elif operation == "exit":
-    print("Are you sure you want to quit \"Calculator\"")
-    leaving = input("Type Y to quit and N go back: ")
-    if leaving == "Y":
-        exit()
-    elif leaving == "N":
-        second_greeting()
-    else:
-        print("Please enter Y or N")
-        second_greeting()
-elif operation == "GCF":
-    x = input("Enter first number: ")
-    y = input("Enter second number: ")
-    GCF(x, y)
-elif operation == "help":
-    help()
-else:
-    print("Error, not a function of the calculator")
-    second_greeting()
+second_greeting()
