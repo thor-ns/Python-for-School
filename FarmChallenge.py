067c294e42475a1e642a951a86d992f2c99a8524
thor-ns <dominicklackovic9@gmail.com>
2024-02-07 11:23:03 -0800

    new file:   FarmChallenge.py

---
+++ FarmChallenge.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self):
+        pass
+class Dog:
+    def __init__(self):
+        pass
+class Sheep:
+    def __init__(self):
+        pass
+class Pig:
+    def __init__(self):
+        pass
+class Horse:
+    def __init__(self):
+        pass
+class Duck:
+    def __init__(self):
+        pass
