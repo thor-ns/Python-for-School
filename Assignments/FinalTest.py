a909ab35174b006e38e30d7904cdde8707d0dd34
thor-ns <dominicklackovic9@gmail.com>
2024-06-11 11:02:52 -0700

    updated files

--- Assignments/FinalTest.py
+++ Assignments/FinalTest.py
@@ -29,11 +29,27 @@ class Triangle(Shapes):
     
     def calcArea(self):
         return ("the area of the triangle is: ", (self.base*self.height)/2)
+
+class Trapezoid(Shapes):
+    def __init__(self, base, height, base2):
+        super().__init__(base, height)
+        self.base2 = base2
     
-base = int(input("What is the base: "))
+    def calcArea(self):
+        return ("the area of the trapezoid is: ", ((self.base + self.base2)/2)*height)
+    
+base1 = int(input("What is the base: "))
+base2 = int(input("What is the second base (trapezoid only): "))
 height = int(input("What is the height: "))
-t1 = Square(base, height)
+
+t1 = Square(base1, height)
+t2 = Trapezoid(base1, height, base2)
+t3 = Triangle(base1, height)
+t4 = Rectangle(base1, height)
 print(t1.calcArea())
+print(t2.calcArea())
+print(t3.calcArea())
+print(t4.calcArea())
 
 #Question 2
 #__________________________________________________________________________________________________________
